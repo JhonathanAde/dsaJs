6c4ad6dcc603a8571ed011635e5e8c5d2b8c6f2d
JhonathanAde <ade.jhonathan24@gmail.com>
2024-01-18 12:23:08 -0500

    completed: twoSum, maxProfit, and containsDuplicate

--- blind75/twoSum_1.js
+++ blind75/twoSum_1.js
@@ -1,5 +1,7 @@
 /* 
-  Two Sum (Easy)
+  HASH MAP TECHNIQUE
+
+  1.) Two Sum (Easy)
   - Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
   • You may assume that each input would have exactly one solution, and you may not use the same element twice.
